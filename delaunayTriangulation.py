6efdfa45f7064799ff2e35eeb09adf84037baebd
飯島緋理 <iijimaakari@iijimaakari.local>
2018-10-29 20:52:59 +0900

    ver.1 by akari

--- delaunayTriangulation.py
+++ delaunayTriangulation.py
@@ -1,1 +1,39 @@
-print("Hello");
+import scipy.stats
+import scipy.spatial
+from numpy.random import RandomState
+import matplotlib.pyplot as plt
+
+#setting
+randomValue=RandomState(100000)
+ax=plt.figure().add_subplot(111)
+
+#coordinate of locations (locations=[[x,...],[y,...]])
+locations=randomValue.randint(0,500,size=(2,100))
+#values of locations (values=[0,0,...])
+values=randomValue.randint(0,1000,size=(100))
+
+
+#triangulation
+triangulation=scipy.spatial.Delaunay(locations.T)
+assimVertex=lambda index: triangulation.points[index]
+triangleSet=map(assimVertex,triangulation.vertices)
+
+#liner
+plt.triplot(locations[0],
+            locations[1],
+            triangles=triangleSet,
+            color='black',
+            linewidth = 0.5)
+
+#triangle color
+index = 0
+for triangle in triangleSet:
+    print(triangle)
+    ax.add_patch(plt.Polygon(triangle,
+                             facecolor='0.3',
+                             alpha=0.5))
+    index+=1
+
+#output
+plt.savefig('triangular.png')
+plt.show()
