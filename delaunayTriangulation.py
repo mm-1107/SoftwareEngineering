4e17995ecab4f7abaa8b8a8ea1391ebf531cd1f7
飯島緋理 <iijimaakari@iijimaakari.local>
2018-11-29 13:54:30 +0900

    ver.5

--- delaunayTriangulation.py
+++ delaunayTriangulation.py
@@ -5,20 +5,43 @@ import matplotlib.pyplot as plt
 import sys
 
 def generateRandomJsonData():
+    """To generate random JsonData.
+    This return array, "locations" and "values".
+    "locations" is two dimention array that have x and y coordinate.
+    "values" is one dimention array that have value of coordinate point
+
+    :param none
+    :return1 locations
+    :rtype array
+    :return2 values
+    :rtype array
+    """
     randomValue = RandomState(100000)
     # coordinate of locations (locations=[[x,...],[y,...]])
     locations = randomValue.randint(0, 500, size=(2, 100))
+    print(type(locations))
     # values of locations (values=[0,0,...])
     values = randomValue.randint(0, 1000, size=(100))
     return locations, values
 
 def getJsonData(jsonFile):
+    """.
+
+    :param jsonFile:This file have x and y dimention and values of coordinate point.
+    :type jsondict
+    :return1 none
+    :return2 generateRandomJsonData()
+    :rtype array
+    """
     if jsonFile:
         return None       
     else:
         return generateRandomJsonData() 
 
 def calculationTriangles(locations):
+    """[Calculation Triangles]
+    Deals with plotting the triangles
+    """
     # triangulation
     triangulation = scipy.spatial.Delaunay(locations.T)
     triangles = triangulation.simplices.copy()
@@ -37,8 +60,12 @@ def detectColor(triangulation, locations, values):
                                  alpha=0.5))
         index += 1
     return ax
-        
+
+
 def plotTriangles(locations, triangles, imageName):
+    """plotTriangles
+    Deals with plotting the triangles
+    """
     # liner
     plt.triplot(locations[0],
                 locations[1],
@@ -50,11 +77,11 @@ def plotTriangles(locations, triangles, imageName):
 
 if __name__ == '__main__':
     
-    args = sys.argv
-    commandType = args[1]
-    jsonFile = args[2]
-    imageName = args[3]
+    #args = sys.argv
+    #commandType = args[1]
+    #jsonFile = args[2]
+    #imageName = args[3]
     locations, values = getJsonData(False)
     triangulation, triangles = calculationTriangles(locations)
     ax = detectColor(triangulation, locations, values)
-    plotTriangles(locations, triangles, imageName)
+    #plotTriangles(locations, triangles, imageName)
