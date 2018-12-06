c5495ae65e94bd5b54e9746fe4a4eef1c7cd9d7e
飯島緋理 <iijimaakari@iijimaakari.local>
2018-12-06 14:42:11 +0900

    ver.7 by akari

--- delaunayTriangulationTest.py
+++ delaunayTriangulationTest.py
@@ -1,1 +1,32 @@
-# test
+import unittest
+from delaunayTriangulation import generateRandomJsonData, getJsonData, calculationTriangles, detectColor, plotTriangles
+
+# This is test file.
+
+class delaunayTriangulation(unittest.TestCase):
+    """test class of delaunayTriangulation.py
+    """
+
+    def test_generateRandomJsonData(self):
+        """test method for generateRandomJsonData
+        """
+
+    def test_getJsonData(self):
+        """test method for getJsonData
+        """
+        
+    def test_calculationTriangles(self):
+        """test method for calculationTriangles
+        """
+
+    def test_detectColor(self):
+        """test method for detectColor
+        """
+
+    def test_plotTriangles(self):
+        """test method for plotTriangles
+        """
+
+if __name__ == "__main__":
+    unittest.main()
+    
