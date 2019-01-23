5ea5c0e8c208258e25fd570cfdfce1d7a89c714b
飯島緋理 <iijimaakari@iijimaakari.local>
2019-01-24 00:32:51 +0900

    Create production data

--- fetchData.py
+++ fetchData.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 def getHTMLcontents(url):
     url = url
@@ -35,20 +36,49 @@ def mapsFormatJson(mainData):
     return mainJson
 
 def populationFormatJson(mainData):
-    i = 0
-    for data in mainData[0]:
-        print(i)
-        print(data)
+    mainJson = []
+    nameList = mainData[0].findAll("",{"class":"name"})
+    ratioList = mainData[0].findAll("td",{"align":"right"})
+    for name in nameList:
+        mainJson.append({"name":name.get_text()[1:]})
+    i=0
+    for ratio in ratioList:
+        if(i%3==0):
+            mainJson[int(i/3)].update({"value":float(ratio.get_text()[:-1])})
         i += 1
-    return 1
+    return mainJson
+
+def getJsonData(path):
+    f = open(path, 'r')
+    jsonData = json.load(f)
+    return jsonData
+
+def createFormatJsonData(mapsData,populationData):
+    jsonData = []
+    
+    for population in populationData:
+        for maps in mapsData:
+            if(maps["name"] == population["name"]):
+                jsonData.append({"x":maps["x"],"y":maps["y"],"value":population["value"]})
+                break
+    return jsonData
 
 
 if __name__ == '__main__':
-     mapsData = getHTMLcontents("http://www.gsi.go.jp/KOKUJYOHO/CENTER/kendata/tokyo_heso.htm")
-     mapsData = mapsData.select('body')
-     mapsJson = mapsFormatJson(mapsData)
-     print(mapsJson)
+    #Create maps data json.
+    #mapsData = getHTMLcontents("http://www.gsi.go.jp/KOKUJYOHO/CENTER/kendata/tokyo_heso.htm")
+    #mapsData = mapsData.select('body')
+    #mapsJson = mapsFormatJson(mapsData)
+    #print(mapsJson)
+
+    #Create population data json.
     #populationData = getHTMLcontents("http://area-info.jpn.org/FornPerPop130001.html")
     #populationData = populationData.select('body')
     #populationJson = populationFormatJson(populationData)
     #print(populationJson)
+
+    #Create required data format
+    mapsJsonData = getJsonData("data/mapsData.json")
+    populationJsonData = getJsonData("data/populationData.json")
+    formatData = createFormatJsonData(mapsJsonData,populationJsonData)
+    print(formatData)
